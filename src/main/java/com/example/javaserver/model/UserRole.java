24baf527960f6deb16ae7224934df5f8792b22e0
Anton Mamakin <antonmamakinscript@gmail.com>
2021-02-17 17:00:04 +0400

    update jwt and class for access control

--- src/main/java/com/example/javaserver/model/UserRole.java
+++ src/main/java/com/example/javaserver/model/UserRole.java
@@ -1,8 +1,17 @@
 package com.example.javaserver.model;
 
 public enum UserRole {
-    USER,
-    TEACHER,
-    ADMIN;
+    USER(0),
+    TEACHER(1),
+    ADMIN(2);
+
+    private int id; // Could be other data type besides int
+    UserRole(int id) {
+        this.id = id;
+    }
+    public int getIdRole(){
+        return id;
+    }
+
 
 }
--- src/main/java/com/example/javaserver/service/Niils.java
+++
@@ -1,4 +0,0 @@
-package com.example.javaserver.service;
-
-public class Niils {
-}
