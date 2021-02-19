935a45bd732628c7d81ba1f66e88e713b198d9e2
pavel <p7ll7p@gmail.com>
2021-02-19 22:38:39 +0400

    added the swaggggger

--- src/main/java/com/example/javaserver/JavaserverApplication.java
+++ src/main/java/com/example/javaserver/JavaserverApplication.java
@@ -4,8 +4,10 @@ import com.example.javaserver.model.User;
 import com.example.javaserver.model.UserRole;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class JavaserverApplication {
 
     public static void main(String[] args) {
