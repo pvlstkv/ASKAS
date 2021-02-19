4b3e9ef0874ed351ed8a8ed450c24ce06194ecf6
pavel <p7ll7p@gmail.com>
2021-02-19 20:31:30 +0400

    updated and added some model (user and subject)

--- src/main/java/com/example/javaserver/model/User.java
+++ src/main/java/com/example/javaserver/model/User.java
@@ -2,25 +2,93 @@ package com.example.javaserver.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "_user")
+//@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String login;
     private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private String phone;
+    private String groupName;
     private UserRole role;
+
+    //    @OneToMany(mappedBy = "", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "subject_id", referencedColumnName = "id")
+    private List<Subject> subjects = new ArrayList<>();
+    //todo to add a gender!!!!!!
 
     public User() {
     }
 
-    public User(Integer id, String login, String password, UserRole role) {
+    public User(Integer id, String login, String password, String email, String firstName, String lastName, String patronymic, String phone, String groupName, UserRole role) {
         this.id = id;
         this.login = login;
         this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+        this.phone = phone;
+        this.groupName = groupName;
         this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
     }
 
     public Integer getId() {
