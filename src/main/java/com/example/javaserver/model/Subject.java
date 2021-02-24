f1209f824468d120c4094f34d3db50d7ba51dbc6
pavel <p7ll7p@gmail.com>
2021-02-24 21:48:19 +0400

    it's work but not enough

--- src/main/java/com/example/javaserver/model/Subject.java
+++ src/main/java/com/example/javaserver/model/Subject.java
@@ -1,16 +1,21 @@
 package com.example.javaserver.model;
 
 
+//import com.example.javaserver.testService.models.Question;
+
+import com.example.javaserver.testService.models.Question;
+
 import javax.persistence.*;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "subjects")
+//@Table(name = "subjects")
 public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String name;
     private String description;
     private OffsetDateTime createdAt;
@@ -18,10 +23,42 @@ public class Subject {
     @ManyToOne(fetch = FetchType.LAZY)
     private User createdBy;
 
-//    @OneToMany(mappedBy = "subject")
-//    private List<Question> question;
+    @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> question = new ArrayList<>();
 
     public Subject() {
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public List<Question> getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(List<Question> question) {
+        this.question = question;
     }
 
     public String getDescription() {
@@ -44,11 +81,11 @@ public class Subject {
         this.name = name;
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
--- src/main/java/com/example/javaserver/service/JwtService.java
+++ src/main/java/com/example/javaserver/service/JwtService.java
@@ -11,11 +11,30 @@ public class JwtService {
     private JwtUtil jwtUtil;
 
     public ContextUser modelAuth(String token){
-        //token = token.substring(7);
         ContextUser contextUser = new ContextUser();
         contextUser.setIdUser(jwtUtil.extractUserId(token));
         contextUser.setRoleUser(jwtUtil.extractUserRole(token));
         return contextUser;
+    }
+
+    public boolean isAdminOrTeacher(ContextUser contextUser) {
+        return isAdmin(contextUser) || isTeacher(contextUser);
+    }
+
+    public boolean isAdminOrTeacherOrStudent(ContextUser contextUser) {
+        return isAdminOrTeacher(contextUser) || isStudent(contextUser);
+    }
+
+    public boolean isAdmin(ContextUser contextUser) {
+        return contextUser.getRoleUser().equals("admin");
+    }
+
+    public boolean isTeacher(ContextUser contextUser) {
+        return contextUser.getRoleUser().equals("teacher");
+    }
+
+    public boolean isStudent(ContextUser contextUser) {
+        return contextUser.getRoleUser().equals("student");
     }
 
 }
