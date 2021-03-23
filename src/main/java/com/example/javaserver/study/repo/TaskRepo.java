d42bb25da9e365fb70a1e35f1cb2dbc43502ea70
Alexey Shishkin <Lech.x32@gmail.com>
2021-03-23 21:24:28 +0400

    UserFile delete fix

--- src/main/java/com/example/javaserver/study/repo/TaskRepo.java
+++ src/main/java/com/example/javaserver/study/repo/TaskRepo.java
@@ -4,12 +4,14 @@ import com.example.javaserver.study.model.Task;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.Set;
 
 public interface TaskRepo extends
         CrudRepository<Task, Long>,
         JpaSpecificationExecutor<Task>
 {
+    @Transactional
     void deleteAllByIdIn(Set<Long> ids);
     Set<Task> findAllByIdIn(Set<Long> ids);
 }
