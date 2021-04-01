cdd61e39082dc3e6a0b91e5d3268af5a5d9c4409
Alexey Shishkin <Lech.x32@gmail.com>
2021-04-01 21:42:55 +0400

    Fixed permanent 502 error and fixed allowedOrigins error

--- src/main/java/com/example/javaserver/general/config/WebConfig.java
+++ src/main/java/com/example/javaserver/general/config/WebConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOriginPatterns("*")/*.allowedOrigins("*")*/.allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH");
+        registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH");
     }
 }
