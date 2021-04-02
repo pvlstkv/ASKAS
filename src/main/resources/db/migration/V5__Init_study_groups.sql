62c61a991dc366782e910f0d6dca0209838afe16
Anton Mamakin <antonmamakinscript@gmail.com>
2021-04-02 21:12:16 +0400

    update migration added 2 study group

--- src/main/resources/db/migration/V5__Init_study_groups.sql
+++ src/main/resources/db/migration/V5__Init_study_groups.sql
@@ -15,3 +15,38 @@ values (111,
         2020,
         (SELECT id FROM departments WHERE short_name LIKE 'ВТ'));
 
+insert into study_groups(code,
+                         course_number,
+                         created_at,
+                         full_name,
+                         group_number,
+                         short_name,
+                         year_of_study_start,
+                         department_id)
+values (111,
+        1,
+        now(),
+        'немецкий язык',
+        1,
+        'ИНЯЗ',
+        2020,
+        (SELECT id FROM departments WHERE short_name LIKE 'ИНЯЗ'));
+
+insert into study_groups(code,
+                         course_number,
+                         created_at,
+                         full_name,
+                         group_number,
+                         short_name,
+                         year_of_study_start,
+                         department_id)
+values (111,
+        1,
+        now(),
+        'радио',
+        1,
+        'р',
+        2020,
+        (SELECT id FROM departments WHERE short_name LIKE 'РФ'));
+
+
