779739b6c7bc1dd334f903e707cd03a3338fea7a
Alexey Shishkin <Lech.x32@gmail.com>
2021-04-15 20:35:19 +0400

    Global validation exception handling

--- src/main/java/com/example/javaserver/study/controller/dto/LiteratureIn.java
+++ src/main/java/com/example/javaserver/study/controller/dto/LiteratureIn.java
@@ -1,14 +1,12 @@
 package com.example.javaserver.study.controller.dto;
 
 import com.example.javaserver.study.model.LiteratureType;
-import com.example.javaserver.study.model.TaskType;
-import com.example.javaserver.study.model.UserFile;
 
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 public class LiteratureIn {
-    @NotNull
+    @NotNull(message = "Необходимо указать тип")
     public LiteratureType type;
     public String title;
     public String authors;
