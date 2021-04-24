fcf52a069337154626065a63d2b0ea471fa3ccc3
Anton Mamakin <antonmamakinscript@gmail.com>
2021-04-24 12:56:32 +0400

    Uhodim

--- src/main/java/com/example/javaserver/conference/controller/dto/ConferenceDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.javaserver.conference.controller.dto;
-
-public class ConferenceDto {
-}
--- src/main/java/com/example/javaserver/conference/controller/mapper/ConferenceMapper.java
+++
@@ -1,4 +0,0 @@
-package com.example.javaserver.conference.controller.mapper;
-
-public interface ConferenceMapper {
-}
