046ac21dd45054aafdea4ae9e6c7fb804299d516
Alexey Shishkin <Lech.x32@gmail.com>
2021-05-28 22:55:10 +0400

    File migrations

---
+++ src/main/resources/db/migration/V15__Init_Files.sql
@@ -0,0 +1,161 @@
+insert into files(access_level, content_length, content_type, link_count, name, user_id)
+values (0, 79360, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 0, 'Task', 1);
+
+insert into files(access_level, content_length, content_type, link_count, name, user_id)
+values (0, 4381641, 'application/pdf', 0, 'Literature', 1);
+
+insert into files(access_level, content_length, content_type, link_count, name, user_id)
+values (0, 784159, 'application/zip', 0, 'Work', 1);
+
+insert into files(access_level, content_length, content_type, link_count, name, user_id)
+values (0, 94333, 'image/jpeg', 0, 'Scheme', 1);
+
+
+
+insert into file_task(task_id, file_id)
+values (1, 1);
+
+insert into file_task(task_id, file_id)
+values (1, 4);
+
+insert into file_task(task_id, file_id)
+values (2, 1);
+
+insert into file_task(task_id, file_id)
+values (2, 4);
+
+insert into file_task(task_id, file_id)
+values (3, 1);
+
+insert into file_task(task_id, file_id)
+values (3, 4);
+
+insert into file_task(task_id, file_id)
+values (4, 1);
+
+insert into file_task(task_id, file_id)
+values (4, 4);
+
+insert into file_task(task_id, file_id)
+values (5, 1);
+
+insert into file_task(task_id, file_id)
+values (5, 4);
+
+insert into file_task(task_id, file_id)
+values (6, 1);
+
+insert into file_task(task_id, file_id)
+values (6, 4);
+
+insert into file_task(task_id, file_id)
+values (7, 1);
+
+insert into file_task(task_id, file_id)
+values (7, 4);
+
+insert into file_task(task_id, file_id)
+values (8, 1);
+
+insert into file_task(task_id, file_id)
+values (8, 4);
+
+insert into file_task(task_id, file_id)
+values (9, 1);
+
+insert into file_task(task_id, file_id)
+values (9, 4);
+
+insert into file_task(task_id, file_id)
+values (10, 1);
+
+insert into file_task(task_id, file_id)
+values (10, 4);
+
+insert into file_task(task_id, file_id)
+values (11, 1);
+
+insert into file_task(task_id, file_id)
+values (11, 4);
+
+insert into file_task(task_id, file_id)
+values (12, 1);
+
+insert into file_task(task_id, file_id)
+values (12, 4);
+
+
+
+insert into file_literature(literature_id, file_id)
+values (1, 2);
+
+insert into file_literature(literature_id, file_id)
+values (2, 2);
+
+insert into file_literature(literature_id, file_id)
+values (3, 2);
+
+insert into file_literature(literature_id, file_id)
+values (4, 2);
+
+insert into file_literature(literature_id, file_id)
+values (5, 2);
+
+insert into file_literature(literature_id, file_id)
+values (6, 2);
+
+
+
+insert into file_work(work_id, file_id)
+values (1, 3);
+
+insert into file_work(work_id, file_id)
+values (2, 3);
+
+insert into file_work(work_id, file_id)
+values (3, 3);
+
+insert into file_work(work_id, file_id)
+values (4, 3);
+
+insert into file_work(work_id, file_id)
+values (5, 3);
+
+insert into file_work(work_id, file_id)
+values (6, 3);
+
+insert into file_work(work_id, file_id)
+values (7, 3);
+
+insert into file_work(work_id, file_id)
+values (8, 3);
+
+insert into file_work(work_id, file_id)
+values (9, 3);
+
+insert into file_work(work_id, file_id)
+values (10, 3);
+
+insert into file_work(work_id, file_id)
+values (11, 3);
+
+insert into file_work(work_id, file_id)
+values (12, 3);
+
+insert into file_work(work_id, file_id)
+values (13, 3);
+
+insert into file_work(work_id, file_id)
+values (14, 3);
+
+insert into file_work(work_id, file_id)
+values (15, 3);
+
+insert into file_work(work_id, file_id)
+values (16, 3);
+
+insert into file_work(work_id, file_id)
+values (17, 3);
+
+insert into file_work(work_id, file_id)
+values (18, 3);
