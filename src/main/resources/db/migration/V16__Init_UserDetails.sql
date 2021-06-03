14314cc56c86f46733d6a9c363b43a168e05e2d2
Alexey Shishkin <Lech.x32@gmail.com>
2021-06-03 15:20:40 +0400

    User details migraytion

---
+++ src/main/resources/db/migration/V16__Init_UserDetails.sql
@@ -0,0 +1,26 @@
+insert into user_contacts(type, value, user_id)
+values ('EMAIL', 'id-anton@gmail.com', 2);
+
+insert into user_contacts(type, value, user_id)
+values ('VKONTAKTE', 'vk.com/antonus-patronus', 2);
+
+insert into user_contacts(type, value, user_id)
+values ('VKONTAKTE', 'vk.com/pashtet', 3);
+
+insert into user_contacts(type, value, user_id)
+values ('TELEGRAM', 't.me/pavel-the-first', 3);
+
+insert into user_contacts(type, value, user_id)
+values ('TELEGRAM', 't.me/alewa', 4);
+
+insert into user_contacts(type, value, user_id)
+values ('TELEGRAM', 't.me/leontiy', 4);
+
+insert into user_contacts(type, value, user_id)
+values ('VKONTAKTE', 'vk.com/mistress-button-architect', 5);
+
+insert into user_contacts(type, value, user_id)
+values ('TELEGRAM', 't.me/foreman', 6);
+
+insert into user_contacts(type, value, user_id)
+values ('EMAIL', 'sale@ritg.ru', 11);
