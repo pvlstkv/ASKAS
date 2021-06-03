55a6c34a427ed982f3ffe1a2e98545ffcc2d70cb
Alexey Shishkin <Lech.x32@gmail.com>
2021-06-03 11:56:55 +0400

    Fixed file migrations

--- src/main/resources/db/migration/V15__Init_Files.sql
+++ src/main/resources/db/migration/V15__Init_Files.sql
@@ -1,14 +1,14 @@
 insert into files(access_level, content_length, content_type, link_count, name, user_id)
-values (0, 15282, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 0, 'Task', 1);
+values (0, 15282, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 12, 'Task', 1);
 
 insert into files(access_level, content_length, content_type, link_count, name, user_id)
-values (0, 4381641, 'application/pdf', 0, 'Literature', 1);
+values (0, 4381641, 'application/pdf', 6, 'Literature', 1);
 
 insert into files(access_level, content_length, content_type, link_count, name, user_id)
-values (0, 784159, 'application/zip', 0, 'Work', 1);
+values (0, 784159, 'application/zip', 18, 'Work', 1);
 
 insert into files(access_level, content_length, content_type, link_count, name, user_id)
-values (0, 94333, 'image/jpeg', 0, 'Scheme', 1);
+values (0, 94333, 'image/jpeg', 12, 'Scheme', 1);
 
 
 
