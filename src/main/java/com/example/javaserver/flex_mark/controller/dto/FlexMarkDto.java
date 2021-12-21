432f843d27a3dfa42054f662d5fb0ec96e6e53b7
pavel astkov <p7ll7p@gmail.com>
2021-12-22 00:28:06 +0400

    Add migration and fix code

--- src/main/java/com/example/javaserver/flex_mark/controller/dto/FlexMarkDto.java
+++ src/main/java/com/example/javaserver/flex_mark/controller/dto/FlexMarkDto.java
@@ -9,8 +9,6 @@ public class FlexMarkDto {
 
     private FMConfigPerCriteriaDto visit;
 
-    private FMConfigPerCriteriaDto taskOnTime;
-
     private FMConfigPerCriteriaDto taskMark;
 
     private FMConfigPerCriteriaDto testMark;
